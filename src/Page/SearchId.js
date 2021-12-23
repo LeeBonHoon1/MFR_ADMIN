854cfadc378ecf417ce42c665a274ce2b6f09a77
Lee-Sangkwon1 <opsizks4@gmail.com>
2021-12-23 21:27:49 +0900

    test

--- src/Page/SearchId.js
+++ src/Page/SearchId.js
@@ -1,11 +1,7 @@
-import React from 'react';
+import React from "react";
 
 export default class SearchId extends React.Component {
   render() {
-    return (
-      <div>
-        SearchId
-      </div>
-    );
+    return <div>SearchId asdkflkasjfkl</div>;
   }
 }
